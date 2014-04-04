786859d193e6f9055a41e3fae0a23703601f031e
Dennis Rippinger <dennis.rippinger@gmail.com>
2014-04-04 17:45:15 +0200

    Crawler skips entry on error

--- spade/spade-crawler/src/main/java/info/interactivesystems/spade/crawler/AmazonRatingCrawler.java
+++ spade/spade-crawler/src/main/java/info/interactivesystems/spade/crawler/AmazonRatingCrawler.java
@@ -14,13 +14,17 @@
  */
 package info.interactivesystems.spade.crawler;
 
+import info.interactivesystems.spade.captcha.CaptchaService;
+import info.interactivesystems.spade.captcha.FileNamer;
 import info.interactivesystems.spade.dao.ShadowReviewDao;
 import info.interactivesystems.spade.entities.ShadowReview;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,11 +38,18 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 @Slf4j
 @Service
 public class AmazonRatingCrawler {
+
+    @Resource
+    private CaptchaService captchaService;
 
     private final WebClient webClient = new WebClient(BrowserVersion.CHROME);
     private final Pattern starPattern = Pattern.compile("\\d[-\\d]*");
@@ -63,11 +74,10 @@ public class AmazonRatingCrawler {
         HtmlPage htmlPage = loadPage(url);
 
         if (htmlPage != null) {
-
             List<ShadowReview> reviewsOfSameProduct = dao.findReviewFromUrl(url);
 
             String stringCategory = "";
-            Double doubleAverageRating = 0.0;
+            Double doubleAverageRating = -1.0;
 
             // Get Category
             List<DomElement> listCategory = (List<DomElement>) htmlPage
@@ -81,20 +91,27 @@ public class AmazonRatingCrawler {
             doubleAverageRating = checkAverageRating(htmlPage, doubleAverageRating);
             doubleAverageRating = checkAverageRatingAlternativeLayout(htmlPage, doubleAverageRating);
 
-            for (ShadowReview review : reviewsOfSameProduct) {
-                review.setType(stringCategory);
-                review.setAverageRating(doubleAverageRating);
+            if (doubleAverageRating != -1.0) {
+                log.info("Average Rating: '{}'", doubleAverageRating);
+                for (ShadowReview review : reviewsOfSameProduct) {
+                    review.setType(stringCategory);
+                    review.setAverageRating(doubleAverageRating);
 
-                dao.save(review);
+                    dao.save(review);
+                }
+            } else {
+                log.warn("Could not retrieve avg. Rating, skipping");
             }
+
         }
+        waitInSeconds(5);
 
     }
 
     @SuppressWarnings("unchecked")
     private Double checkAverageRating(HtmlPage htmlPage, Double doubleAverageRating) {
         List<DomElement> listAverageCustomerRating;
-        if (doubleAverageRating.equals(0.0)) {
+        if (doubleAverageRating.equals(-1.0)) {
             listAverageCustomerRating = (List<DomElement>) htmlPage
                 .getByXPath("//i[@class]");
             if (!listAverageCustomerRating.isEmpty()) {
@@ -113,14 +130,13 @@ public class AmazonRatingCrawler {
                 }
 
             }
-            return 0.0;
         }
         return doubleAverageRating;
     }
 
     @SuppressWarnings("unchecked")
     private Double checkAverageRatingAlternativeLayout(HtmlPage htmlPage, Double doubleAverageRating) {
-        if (doubleAverageRating.equals(0.0)) {
+        if (doubleAverageRating.equals(-1.0)) {
             List<DomElement> listAverageCustomerRating = (List<DomElement>) htmlPage.getByXPath("//span[@title]");
             if (!listAverageCustomerRating.isEmpty()) {
                 DomElement averageCustomerRating = listAverageCustomerRating
@@ -142,21 +158,53 @@ public class AmazonRatingCrawler {
      */
     private HtmlPage loadPage(String currentProductPage) {
         HtmlPage page = loadPageRaw(currentProductPage);
-        if (page.getTitleText().equals("Robot Check")) {
-            webClient.getCookieManager().clearCookies();
-            randomWalk(page);
-            return loadPageRaw(currentProductPage);
+        if (page.getTitleText().contains("Robot Check")) {
+            log.info("Detected as Robot, tring to break captcha");
+            waitInSeconds(10);
+            HtmlPage breakCaptchaResult = breakCaptcha(page);
+
+            if (breakCaptchaResult.getTitleText().contains("Robot Check")) {
+                log.info("Captcha break failed, reseting cookies");
+                webClient.getCookieManager().clearCookies();
+                randomWalk();
+                return loadPageRaw(currentProductPage);
+            } else {
+                log.info("Captcha break succesfull");
+                return breakCaptchaResult;
+            }
+
         }
         return page;
 
     }
 
+    private HtmlPage breakCaptcha(HtmlPage page) {
+        HtmlForm form = page.getFormByName("");
+
+        String captchaString = captchaService.getValueFromCaptcha(page);
+
+        HtmlButton button = (HtmlButton) page.getByXPath("//button[@class]").get(0);
+        HtmlTextInput textField = form.getInputByName("field-keywords");
+
+        textField.setValueAttribute(captchaString);
+        try {
+            HtmlPage result = button.click();
+            return result;
+        } catch (IOException e) {
+            log.error("Could not click on Captcha button", e);
+        }
+        return page;
+    }
+
     @SuppressWarnings("unchecked")
-    private void randomWalk(HtmlPage page) {
+    private void randomWalk() {
+        HtmlPage page = loadPageRaw("http://www.amazon.com");
         List<DomElement> listOfLinks = (List<DomElement>) page.getByXPath("//a[@href]");
         Collections.shuffle(listOfLinks);
-        for (Integer i = 0; i >= 4; i++) {
+        for (Integer i = 0; i >= 10; i++) {
+            waitInSeconds(5);
             DomElement domUrl = listOfLinks.get(i);
+            log.info("Surfing to '{}'", domUrl);
             String relativeUrl = domUrl.getAttribute("href");
             loadPageRaw("http://www.amazon.com" + relativeUrl);
         }
@@ -166,6 +214,7 @@ public class AmazonRatingCrawler {
         HtmlPage page = null;
         try {
             page = webClient.getPage(currentProductPage);
+            log.info("Page Title: '{}'", page.getTitleText());
         } catch (FailingHttpStatusCodeException e1) {
             log.error(e1.getMessage());
         } catch (MalformedURLException e1) {
@@ -175,5 +224,13 @@ public class AmazonRatingCrawler {
         }
 
         return page;
+    }
+
+    private void waitInSeconds(Integer seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
