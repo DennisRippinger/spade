dbe13fe86025a97f981d2d5a4b02c7217d65f094
Dennis Rippinger <dennis.rippinger@gmail.com>
2014-04-16 22:10:56 +0200

    Information Density component added

---
+++ spade/spade-core/src/main/java/info/interactivesystems/spade/sentence/InfoDensityIndex.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2014 Dennis Rippinger
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package info.interactivesystems.spade.sentence;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * The Class InfoDensityIndex provides methods to calculate the information density of a review.
+ * 
+ * @author Dennis Rippinger
+ */
+@Slf4j
+@Component
+public class InfoDensityIndex {
+
+    /**
+     * Calculates the information density with the GZIP algorithm.
+     * 
+     * @param review a review as text.
+     * @return the density of the text.
+     */
+    public Double getInformationDensity(String review) {
+
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+            GZIPOutputStream gzip = new GZIPOutputStream(out);
+            gzip.write(review.getBytes());
+            gzip.close();
+
+            Double compressed = out.size() * 1.0;
+            Double uncompressed = review.getBytes().length * 1.0;
+
+            Double result = compressed / uncompressed;
+
+            if (result > 1.0) {
+                result = 1.0;
+            }
+            return result;
+
+        } catch (IOException e) {
+            log.warn("Could not Compress the Input String", e);
+        }
+        return 0.0;
+
+    }
+
+}
